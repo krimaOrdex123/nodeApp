088d4ccd1b07d66e246cc1832ad173d7a445babf
rushabhOrdex <108253015+enapatel0706@users.noreply.github.com>
2023-04-05 16:17:59 +0530

    nodeApp

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/signup',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var password = req.body.password;
+
+    var data = {
+        "name": name,
+        "email" : email,
+        "phno": phno,
+        "password" : password
+    }
+
+    db.collection('details').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('success.html')
+
+})
+
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('signup.html');
+}).listen(4500);
+
+
+console.log("Listening on PORT 4500");
